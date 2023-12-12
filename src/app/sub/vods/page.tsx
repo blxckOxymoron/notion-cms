5c6ce4c023e38aae6bf5f2efec15689a93c6ffba
Oxymoron <daniel.m.bihlmaier@gmail.com>
2023-12-12 19:40:37 +0100

    ✨ search input

--- src/app/sub/vods/page.tsx
+++ src/app/sub/vods/page.tsx
@@ -1,13 +1,29 @@
 import { getNotionPages } from "@/data/notionPages";
 import LoadMoreButton from "./LoadMoreButton";
 import VODWindow from "./VODWindow";
+import searchIcon from "./search.svg";
+import Image from "next/image";
 
 export default async function VODPage() {
   const pages = await getNotionPages();
 
   return (
     <div className="flex flex-col items-center p-4 gap-4">
-      <header>search</header>
+      <header>
+        <form className="flex gap-1 rounded-full px-3 py-1 border border-white">
+          <Image
+            src={searchIcon}
+            alt="Search Icon"
+            className="text-white"
+            style={{ color: "inherit" }}
+          />{" "}
+          <input
+            type="search"
+            name="q"
+            className="bg-transparent rounded-sm px-2 focus:outline-none"
+          />
+        </form>
+      </header>
       <main className="flex flex-col gap-4 items max-w-[1700px] ">
         <h1 className="text-7xl">WELCOME TO THE VODS</h1>
         <div className="flex flex-wrap justify-center gap-4">
