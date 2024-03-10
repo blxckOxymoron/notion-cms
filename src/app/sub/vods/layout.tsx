4112ef87660cd3003e17f13e2cbed1c2d8831a17
Oxymoron <71148001+blxckOxymoron@users.noreply.github.com>
2024-03-10 11:14:38 +0100

    🐛 heder icons disappear

--- src/app/sub/vods/layout.tsx
+++ src/app/sub/vods/layout.tsx
@@ -10,25 +10,25 @@ export default function VODLayout({ children }: { children: React.ReactNode }) {
       <header className="flex gap-2 justify-center">
         <a
           href={Routes.vods.all}
-          className="p-2 leading-snug rounded-full border border-white hover:bg-white/30 transition-colors"
+          className="p-2 leading-snug rounded-full shrink-0 border border-white hover:bg-white/30 transition-colors"
         >
           <Image src={homeIcon} alt="Home Icon" />
         </a>
         <form
-          className="flex gap-1 rounded-full px-3 py-1 border border-white"
+          className="flex gap-1 rounded-full px-3 py-1 border border-white flex-1"
           action={Routes.vods.all}
         >
           <Image src={searchIcon} alt="Search Icon" />
           <input
             type="search"
             name="q"
-            className="bg-transparent rounded-sm px-2 focus:outline-none"
+            className="bg-transparent rounded-sm px-2 w-full min-w-0 focus:outline-none"
             placeholder="Suche ..."
           />
         </form>
         <a
           href={Routes.auth.info}
-          className="p-2 leading-snug rounded-full border border-white hover:bg-white/30 transition-colors"
+          className="p-2 leading-snug rounded-full shrink-0 border border-white hover:bg-white/30 transition-colors"
         >
           <Image src={twitchIcon} alt="Twitch Icon" />
         </a>
