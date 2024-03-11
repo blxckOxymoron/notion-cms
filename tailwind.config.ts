7af6987d80477e54de9a6406d0aa1ce28b2a395a
Oxymoron <71148001+blxckOxymoron@users.noreply.github.com>
2024-03-11 22:56:33 +0100

    💄 red button color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        primary: "#3b82f6",
+        primary: "#dc2626",
       },
     },
   },
